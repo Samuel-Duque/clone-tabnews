4f0f25f4b68b8412c22b8e477660226e367b244f
Samuel L P Duque <samuel.locatel@souunit.com.br>
2024-06-17 20:40:41 +0000

    teste de integração

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
+import { setHttpClientAndAgentOptions } from "next/dist/server/config";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Sou acima da média" });
+  const updatedAt = new Date().toISOString();
+  const versionPost = await pool.query("SHOW server_version;");
+  const postgresV = versionPost.rows[0].server_version;
+
+  const MaxConnections = await pool.query("SHOW max_connections");
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresV,
+    max_connections: MaxConnections,
+  });
 }
 
 export default status;
